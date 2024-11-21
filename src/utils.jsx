4c8e79a0c0105a311cbc5d209baf4bcc6934850c
Ferdy Sopian <pinoan42@gmail.com>
2024-11-21 14:53:20 +0800

    Update to support GitHub Actions workflow for CI/CD

--- src/utils.jsx
+++ src/utils.jsx
@@ -1,12 +1,21 @@
 import axios from 'axios'
 import { parse as parseDomain } from 'tldts'
+const apiUrl = import.meta.env.VITE_BASE_API_URL
+
+const axiosInstance = axios.create({
+    baseURL: apiUrl,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
 
 export const generateCriticalCSS = async (url, mobileViewport, desktopViewport, removeFontFace) => {
     const parsedDomain = parseDomain(url)
     // if hostname is localhost, domain will be localhost
     let domain = parsedDomain?.domain || ''
     // if node env is development, domain will be localhost
-    if (process.env.NODE_ENV !== 'production') {
+    if (import.meta.env.MODE !== 'production') {
         if (parsedDomain?.hostname === 'localhost') {
             domain = 'localhost'
         }
@@ -33,16 +42,21 @@ export const generateCriticalCSS = async (url, mobileViewport, desktopViewport, 
     }
 
     try {
-        const response = await axios.post('http://localhost:3000/api/generate', { ...data })
+        const response = await axiosInstance.post('/api/generate', { ...data })
         return response.data
     } catch (error) {
         let errorMessage = ''
         // Check if error is from Axios
         if (axios.isAxiosError(error)) {
-            console.error('Axios error generating critical CSS:', error.response?.data || error.message)
+            // if production, show generic error message
+            if (import.meta.env.MODE !== 'production') {
+                console.error('Unexpected error generating critical CSS:', error.response?.data || error.message)
+            }
             errorMessage = error.response?.data.error || error.message
         } else {
-            console.error('Unexpected error generating critical CSS:', error)
+            if (import.meta.env.MODE !== 'production') {
+                console.error('Unexpected error generating critical CSS:', error)
+            }
             errorMessage = 'Failed to generate critical CSS'
         }
         throw new Error(errorMessage)
